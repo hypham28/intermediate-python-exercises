410d7325a04a3e3b1a09e70003a2b0f05699973f
hypham28 <143453496+hypham28@users.noreply.github.com>
2023-09-04 00:21:25 -0400

    adding exercise 1 from intermediate

---
+++ intermediateex1.py
@@ -0,0 +1,19 @@
+def unique_elements(input_list):
+    # Create an empty list to store unique elements
+    unique_list = []
+
+    # Iterate through the input list
+    for item in input_list:
+        # If the item is not in the unique list, add it
+        if item not in unique_list:
+            unique_list.append(item)
+
+    return unique_list
+
+# Test the function with a sample list
+sample_list = [3, 14, 15, 2, 3, 6, 8]
+result = unique_elements(sample_list)
+
+# Print the result
+print("Original list:", sample_list)
+print("List with unique elements:", result)
