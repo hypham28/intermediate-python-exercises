7047e0ed07af4b8f6180c45ce65e75a929d57be5
hypham28 <143453496+hypham28@users.noreply.github.com>
2023-09-07 00:44:38 -0400

    Update intermediateex3.py

--- intermediateex3.py
+++ intermediateex3.py
@@ -1,11 +1,11 @@
 def count_lowercase_letters(input_string):
-    # Initialize an empty dictionary to store letter counts
+    # start an empty dictionary to store letter counts
     letter_counts = {}
 
     # Remove spaces and convert the input string to lowercase
     cleaned_string = input_string.replace(" ", "").lower()
 
-    # Iterate through the cleaned string
+    # go through cleaned string
     for char in cleaned_string:
         if char.isalpha():  # Check if the character is a letter
             if char in letter_counts:
@@ -18,7 +18,7 @@ def count_lowercase_letters(input_string):
 # Get input from the user
 user_input = input("Enter a string: ")
 
-# Call the function and get the letter counts
+# Call function
 result_dict = count_lowercase_letters(user_input)
 
 # Print the resulting dictionary
