8b069112740eeff17904ddbee7b3cf41d9b09da5
hypham28 <143453496+hypham28@users.noreply.github.com>
2023-09-07 00:44:08 -0400

    Update intermediateex2.py

--- intermediateex2.py
+++ intermediateex2.py
@@ -1,7 +1,7 @@
 def combine_dicts(dict1, dict2):
     result_dict = {}
     
-    # Iterate through the keys in dict1
+    # go through dict.1
     for key, value in dict1.items():
         if key in dict2:
             # If the key exists in both dicts, sum the values
@@ -9,7 +9,7 @@ def combine_dicts(dict1, dict2):
 
     return result_dict
 
-# Test the function with two sample dictionaries
+# Test 
 dict1 = {"apple": 3, "banana": 2, "cherry": 1}
 dict2 = {"apple": 2, "banana": 4, "date": 5}
 
